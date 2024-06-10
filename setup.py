9555704f386df826dc7010ad566df188fe9cadfb
parm <160158033+prmshra@users.noreply.github.com>
2024-06-10 15:42:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,23 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='singlecelldata',
+    version='0.1.0',
+    description='API for handling single-cell RNA-seq, hyperspectral, and brightfield data',
+    author='Parmita Mishra',
+    author_email='parm@precigenetics.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas',
+        'anndata',
+        'tifffile',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
